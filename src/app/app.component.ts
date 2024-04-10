3dcf3a56264435016398018641be170d557b6277
Vishva Bhatt <bhattvishva25@gmail.com>
2024-04-10 09:44:31 +0000

    Runnable Simple Angular Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public imageUrl = '';
-  constructor() {}
-  public updateImageUrl($event: string) {
-    this.imageUrl = `../assets/${$event}.jpg`;
+
+  constructor() {
+    
   }
 }
