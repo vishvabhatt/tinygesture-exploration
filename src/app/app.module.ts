2c58d1f0bc9e919847fe4f072ee6bbf788447b57
Vishva Bhatt <bhattvishva25@gmail.com>
2024-04-10 10:06:39 +0000

    include gesture module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GestureModule } from 'src/gesture/gesture.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CommonModule, BrowserModule],
+  imports: [CommonModule, BrowserModule, GestureModule],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
